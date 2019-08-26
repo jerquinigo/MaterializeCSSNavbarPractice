18c7c4f3c9c1c750bfc23309a2e62e4bfd10828f
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-08-26 15:03:21 -0400

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>the navbar</div>;
+  }
+}
+
+export default Navbar;
